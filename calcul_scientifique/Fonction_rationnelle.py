140d003107ed729dc0bb545814bf09196bd50145
Sparthanesai <sparthanesai@gmail.com>
2026-06-25 02:13:31 +0100

    Commit général

--- calcul_scientifique/Fonction_rationnelle.py
+++ calcul_scientifique/Fonction_rationnelle.py
@@ -2,34 +2,29 @@ import sympy as sp
 import numpy as np  
 import matplotlib.pyplot as plt 
  
- # Définissons la fontion symbolique
+ # Définissons la fonction symbolique
 x = sp.symbols('x')
 f = (2*x**2 - 3)/(x-1)
 f_prime = sp.diff(f, x)
 points_critiques = sp.solve(f_prime, x, domain=sp.S.Reals)
-
 # Convertissons les fonctions symboliques en fonctions numériques
 f_num = sp.lambdify(x, f, modules='numpy')
 f_prime_num = sp.lambdify(x, f_prime, modules='numpy')
-
-# Séquençàns les différentes valeurs pour respecter le domaine de définition de la fonction qui est x =! 1
+# Séquençons les différentes valeurs pour respecter le domaine de définition de la fonction qui est x =! 1
 x1 = np.linspace(-4, 1, 500, endpoint=False)
 x2 = np.linspace(1, 4, 300)[1:]
 x = np.concatenate((x1, x2))
 # Trouvons les valeurs par chacune des fonctions des x 
 y = f_num(x)
 y_prime = f_prime_num(x)
-
 # Récupérons les valeurs des points critiques et trouvons leurs images par  la fonction f
 x_crit = np.array([ float(p) for p in points_critiques if p.is_real])
 y_crit = f_num(x_crit)
-
 # Initialisation matplotlib
 plt.figure()
 plt.plot(x, y, color='blue', label="f(x)=(2*x**2 - 3)/(x-1)")
 plt.plot(x, y_prime, '--', color='red', label="f'(x)=4*x/(x - 1) - (2*x**2 - 3)/(x - 1)**2")
 plt.scatter(x_crit, y_crit, s=20, label="Points critiques")
-
 # Boucle pour afficher les coordonnées des points au dessus des points crtitiques
 for xc, yc in zip(x_crit, y_crit):
     plt.annotate(f"f({xc:.0f}, {yc:0.fcat})", (xc, yc), xytext=(10, 10), textcoords='offset points')
